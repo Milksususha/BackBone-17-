695046c97405536881aa529640bb10a94b949275
Ксения <milksusha111@mail.ru>
2016-11-16 16:54:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+﻿$(function() {
+	//пространство имён
+	window.App = {
+		Views: {},
+		Models: {},
+		Collections:{},
+		Router:{}
+		
+	};
+	//шаблон
+	window.template = function(id) {
+	   return _.template( $('#' + id).html() );
+	};
+
+	App.Router = Backbone.Router.extend({
+	   routes: {
+          ''     : 'index',
+		  'read' : 'read'
+	   },
+	   
+       index: function(){
+	      console.log('Всем привет от индексного роута!');
+	   },
+     
+	 read: function(){
+		 console.log('это роут Read');
+	  }	   
+	});
+	
+new App.Router();	
+Backbone.history.start();
+
+
+})();
